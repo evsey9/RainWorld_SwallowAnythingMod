a16e23b3974e7b65562b17c68be37d56ddffeceb
evsey9 <evseyru1@mail.ru>
2021-07-18 01:27:32 +0500

    added mod files

---
+++ SwallowAnythingMod/SwallowAnythingMod.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BepInEx;
+
+namespace SwallowAnythingMod
+{
+    [BepInPlugin("drwoof.swallowanythingmod", "Swallow Anything Mod", "0.1.0")]
+    public class SwallowAnythingMod : BaseUnityPlugin
+    {
+        public SwallowAnythingMod()
+        {
+            CanBeSwallowedPatch.Patch();
+            ReleaseGraspPatch.Patch();
+        }
+    }
+}
